d1f0dedc7e313d64bf55832b2814d9de653aa855
Rahul coral <100202443+Rahulcse79@users.noreply.github.com>
2024-04-17 13:22:10 +0530

    Inheritance_revision

---
+++ Java_revision/Oops_revision/Inheritance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class vehicles{
+
+public:
+    string name;
+
+    vehicles(name)
+    {
+        this->name = name;
+    }
+
+    void getName()
+    {
+        cout<<this->name<<endl;
+    }
+
+protected:
+    int password;
+
+};
+
+class car public vehicles{
+    
+};
+
+int main()
+{
+    
+
+    return 0;
+}
