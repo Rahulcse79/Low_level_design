6291727346fe9fd0432e6bd2e2b4941ec0914e9d
Rahul coral <100202443+Rahulcse79@users.noreply.github.com>
2024-04-18 14:23:39 +0530

    revision

--- Java_revision/Oops_revision/Inheritance.cpp
+++ Java_revision/Oops_revision/Inheritance.cpp
@@ -1,33 +1,67 @@
 #include <iostream>
 using namespace std;
 
-class vehicles{
-
-public:
-    string name;
-
-    vehicles(name)
-    {
-        this->name = name;
-    }
-
-    void getName()
-    {
-        cout<<this->name<<endl;
-    }
+class vehicle{
 
 protected:
-    int password;
+    string name;
 
+public:
+    string model;
+    int noOfTyres;
+
+    string getName()
+    {
+        return this->name;
+    }
+
+    vehicle(string name, string model, int noOfTyres)
+    {
+        cout<<"I am inside vehicle ctor"<<endl;
+        this->name = name;
+        this->model = model;
+        this->noOfTyres = noOfTyres;
+    }
+
+    void start_engine()
+    {
+        cout<<"Engine is starting "<< name << " " << model << endl;
+    }
+
+    void stop_engine()
+    {
+        cout<<"Engine is stoping "<< name << " "<< model <<endl;
+    }
 };
 
-class car public vehicles{
-    
+class Car: public vehicle
+{
+
+public:
+    int noOfDoors;
+    string transmissionType;
+
+    Car(string name, string model, int noOfTyres, int noOfDoors, string transmissionType):vehicle(name,model,noOfTyres)
+    {
+        this->noOfDoors = noOfDoors;
+        this->transmissionType = transmissionType;
+    }
+
+    void startAc()
+    {
+        cout<< "Ac has started of "<< name <<endl;
+    }      
 };
 
 int main()
 {
-    
+    Car A("Maruti 800", "LXI", 4, 4, "Manual");
+
+    cout<<"Name: "<<A.getName()<<endl;
+    cout<<"Model: "<<A.model<<endl;
+    cout<<"NoOfTyres: "<<A.noOfTyres<<endl;
+    cout<<"NoOfDoors: "<<A.noOfDoors<<endl;
+    cout<<"TransmissionType: "<<A.transmissionType<<endl; 
 
     return 0;
 }
